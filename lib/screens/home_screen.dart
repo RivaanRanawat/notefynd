4de59ae30d0bd75df6bf3a00b50dbc15bfe78e91
Rivaan Ranawat <namanrivaan@gmail.com>
2021-04-18 01:59:48 +0530

    designing creator plus icon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -103,11 +103,52 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavigationBarItem(
               icon: Icon(Icons.search, size: 30), label: ""),
           if (status == "creator")
-            BottomNavigationBarItem(icon: Icon(Icons.add, size: 30), label: ""),
+            BottomNavigationBarItem(icon: customIcon(), label: ""),
           BottomNavigationBarItem(
               icon: Icon(Icons.article, size: 30), label: ""),
           BottomNavigationBarItem(
               icon: Icon(Icons.person, size: 30), label: ""),
+        ],
+      ),
+    );
+  }
+
+  customIcon() {
+    return Container(
+      width: 45.0,
+      height: 30.0,
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 10.0),
+            width: 38,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 250, 45, 108),
+              borderRadius: BorderRadius.circular(7.0),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            width: 38,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 32, 211, 234),
+              borderRadius: BorderRadius.circular(7.0),
+            ),
+          ),
+          Center(
+            child: Container(
+              height: double.infinity,
+              width: 38,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(7.0)),
+              child: Icon(
+                Icons.add,
+                color: Colors.black,
+                size: 20.0,
+              ),
+            ),
+          ),
         ],
       ),
     );
