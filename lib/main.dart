ba7f22b6d12b9ebcc73b594a8c95a16ed3e33344
Rivaan Ranawat <rivaanranawat@Rivaans-MacBook-Air.local>
2021-05-24 16:00:29 +0530

    adding support of web

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +14,9 @@ import 'package:provider/provider.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  
   runApp(
     MultiProvider(
       providers: [
